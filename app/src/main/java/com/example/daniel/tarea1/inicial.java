02b6e803801912983ae051cc24581900d37696ab
fabiodaniel <benitodalecandela@gmail.com>
2016-12-09 14:34:31 -0400

    añadiendo imagenes al proyecto

---
+++ app/src/main/java/com/example/daniel/tarea1/inicial.java
@@ -0,0 +1,13 @@
+package com.example.daniel.tarea1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class inicial extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicial);
+    }
+}
